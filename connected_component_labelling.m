ed9d2898a860f610bb508ee347b27dd7f9e67584
mustfkeskin <mustfkeskin@users.noreply.github.com>
2017-03-27 21:22:23 +0300

    Update connected_component_labelling.m

--- connected_component_labelling.m
+++ connected_component_labelling.m
@@ -1,22 +1,32 @@
 clear;
-I = imread('labirent.pgm');
+I = imread('semboller.pgm');
 
 [M, N] = size(I);
 labels = zeros(M,N);
 
 currentLabel = 0;
+
+% 8 komsu için
 dx = [-1, 0, 1, 1,  1,  0, -1, -1];
 dy = [ 1, 1, 1, 0, -1, -1, -1,  0];
 
-for i = 2:M - 1
-    for j = 2:N - 1
-        komsular = zeros(1,8,'double');
-        for k = 1:8
+
+% 4 komsu icin
+%dx = [0, 1,  0, -1];
+%dy = [1, 0, -1,  0];
+
+for i = 2 : M - 1
+    for j = 2 : N - 1
+        
+        komsular = zeros(1,length(dx),'double');
+        for k = 1:length(dx)
           komsular(1,k) = labels(i + dx(k), j + dy(k));
         end
         
+        
+        
         if(sum(komsular) == 0 && I(i, j) == 0)
-            currentLabel = currentLabel + 10;
+            currentLabel = currentLabel + 1;
             labels(i ,j) = currentLabel;   
         end
         
@@ -25,8 +35,11 @@ for i = 2:M - 1
             if I(i,j) == 0
                 labels(i,j) = label;
             end
-            for k = 1:8 
-               if  I(i + dx(k), j + dy(k)) == 0 && labels(i + dx(k), j + dy(k)) > 0
+            for k = 1:length(dx) 
+               if (I(i, j) == 0 && ...
+                   labels(i + dx(k), j + dy(k)) > 0 && ...
+                   labels(i + dx(k), j + dy(k)) ~=label)
+               
                    search = labels(i + dx(k), j + dy(k));
                    labels(labels == search) = label;
                end
